dac63bf5c66818000ff2dfe95ef6e64aec6a7b54
JC Renner <jc-renner@lu-tec.com>
2021-10-16 19:18:25 -0400

    created intakeContinue function, added more questions

--- index.js
+++ index.js
@@ -9,10 +9,34 @@ const questions = require('./lib/questions')
 
 intake = () => {
     inquirer.prompt(questions.managerQs)
-    .then(({name, id}) => {
-        const manager = new Manager(name, id);
-        console.log(manager)
+    .then(({name, id, email, officeNum}) => {
+        const manager = new Manager(name, id, email, officeNum);
+        console.log(manager);
+        // now ask who they wanna create
+
     });
 };
+
+intakeContinue = () => {
+    inquirer.prompt(questions.continue)
+    .then((response) => {
+        switch (response.continue) {
+            case "Engineer":
+                intakeEngineer();
+                break;
+            case "Intern":
+                intakeIntern();
+                break;
+            case "I'm finished adding employees.":
+                console.log('Thank you! Your team web page is being created.')
+                createTeamPage();
+                break;
+        }
+    })
+}
+
+createTeamPage = () => {
+
+}
 
 intake();
