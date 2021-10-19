4bb839328b55edce0ed7869019e262c7619f729c
JC Renner <jc-renner@lu-tec.com>
2021-10-18 21:37:03 -0400

    intern questions, intakeContinue questions, intake functions, arrays, successfully pushed answers into arrays

--- index.js
+++ index.js
@@ -4,23 +4,47 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const questions = require('./lib/questions')
+const questions = require('./lib/questions');
+
+const allClassified = [];
+const managerObj = [];
+const engineerObjs = [];
+const internObjs = [];
 
 
 intake = () => {
     inquirer.prompt(questions.managerQs)
     .then(({name, id, email, officeNum}) => {
-        const manager = new Manager(name, id, email, officeNum);
-        console.log(manager);
-        // now ask who they wanna create
-
+        const managerInstance = new Manager(name, id, email, officeNum);
+        managerObj.push(managerInstance);
+        console.log(managerObj);
+        intakeContinue();
     });
 };
+
+intakeEngineer = () => {
+    inquirer.prompt(questions.engineerQs)
+    .then(({name, id, email, github}) => {
+        const engineerInstance = new Engineer(name, id, email, github);
+        engineerObjs.push(engineerInstance);
+        intakeContinue(); 
+    })
+}
+
+intakeIntern = () => {
+    inquirer.prompt(questions.internQs)
+    .then(({name, id, email, school}) => {
+        const internInstance = new Intern(name, id, email, school);
+        internObjs.push(internInstance);
+        intakeContinue(); 
+    })
+}
+
 
 intakeContinue = () => {
     inquirer.prompt(questions.continue)
     .then((response) => {
-        switch (response.continue) {
+        switch (response.add) {
             case "Engineer":
                 intakeEngineer();
                 break;
@@ -31,18 +55,27 @@ intakeContinue = () => {
                 console.log('Thank you! Your team web page is being created.')
                 createTeamPage();
                 break;
+            default: console.log(response.add); console.log('default triggered');
         }
     })
 }
 
 createTeamPage = () => {
-    fs.writeFile("aboutus.html",team, (err) => {
-      if(err) {
-        throw err;
-      };
-      console.log("Your team has been constructed!");
-    });
-    open("team.html");
+    // fs.writeFile("aboutus.html",team, (err) => {
+    //   if(err) {
+    //     throw err;
+    //   };
+    //   console.log("Your team has been constructed!");
+    // });
+    // open("team.html");
+
+    internObjs.push(allClassified);
+    engineerObjs.push(allClassified);
+    managerObj.push(allClassified);
+
+    console.log(internObjs, engineerObjs, managerObj);
+    console.log(allClassified);
+
 };
 
 intake();
